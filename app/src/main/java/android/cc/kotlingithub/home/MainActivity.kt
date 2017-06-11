226e6797776ae893eb08b55feb64b84713b9993d
Adit Lal <aditlal90@gmail.com>
2017-06-11 19:10:19 +0530

    Init Session code

--- app/src/main/java/android/cc/kotlingithub/home/MainActivity.kt
+++ app/src/main/java/android/cc/kotlingithub/home/MainActivity.kt
@@ -1,13 +1,34 @@
 package android.cc.kotlingithub.home
 
 import android.cc.kotlingithub.R
+import android.cc.kotlingithub.network.data.Items
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainContract.View {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun showRepos(list: List<Items?>?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showProgress() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun hideProgress() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onFailure(errorMsg: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showEmpty() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
--- app/src/main/java/android/cc/kotlingithub/network/Api.kt
+++ app/src/main/java/android/cc/kotlingithub/network/Api.kt
@@ -1,9 +1,9 @@
 package android.cc.kotlingithub.network
 
-import retrofit2.Response
+import android.cc.kotlingithub.network.data.Response
+import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
-import rx.Observable
 
 /**
  * Created by aditlal 11/06/17
@@ -12,5 +12,5 @@ import rx.Observable
 interface Api {
 
     @GET("search/repositories")
-    fun getRepos(@Query("q") query: String, @Query("per_page") per_page: Int): Observable<Response<*>>
+    fun getRepos(@Query("q") query: String, @Query("per_page") per_page: Int): Observable<Response>
 }
